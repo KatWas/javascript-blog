db41b56b1a2ee2bac4758f7e007e91c87ff2b292
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-09-07 13:42:21 +0200

    Add sass

--- js/script.js
+++ js/script.js
@@ -2,14 +2,31 @@
     'use strict';
         function titleClickHandler(event) {
             event.preventDefault();
+        
         /* remove class 'active' from all article links */
         const activeLinks = document.querySelectorAll(" .titles a.active ");
         for (let activeLink of activeLinks) {
             activeLink.classList.remove("active");
         }    
+        
         /* add class 'active' to the clicked link */
         const clickedElement = this;
-        clickedElement.classList.add ("active");
-
+        clickedElement.classList.add("active");
+        
+        /* remove class 'active' from all articles */
+        const activeArticles = document.querySelectorAll(" .post ");
+        for (let activeArticle of activeArticles) {
+            activeArticle.classList.remove("active");
         }
+        /* get 'href' attribute from the cliked link */
+        const articleSelector = clickedElement.getAttribute("href");
+
+        /* find the correct article using the selector (value of 'href' attribute) */
+        const targetArticle = document.querySelector(articleSelector);
+
+        /* add class 'active' to the correct article */
+        targetArticle.classList.add("active");
+        }
+        /* nowa funkcja */
+        
 }
